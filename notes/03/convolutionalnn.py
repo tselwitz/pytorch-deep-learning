740f9547231291d6631ce71ee0fecf5818752381
Todd Selwitz <selwitz.todd@gmail.com>
2023-10-06 14:35:42 -0600

    convolution pt 1

--- notes/03/convolutionalnn.py
+++ notes/03/convolutionalnn.py
@@ -87,4 +87,151 @@ torch.manual_seed(42)
 model_2 = FashionMNISTModelV2(
     input_shape=1, hidden_units=10, output_shape=len(classes)
 ).to(device)
-model_2
+print(model_2)
+
+torch.manual_seed(42)
+
+# Create sample batch of random numbers with same size as image batch
+images = torch.randn(
+    size=(32, 3, 64, 64)
+)  # [batch_size, color_channels, height, width]
+test_image = images[0]  # get a single image for testing
+# print(
+# f"Image batch shape: {images.shape} -> [batch_size, color_channels, height, width]"
+# )
+# print(f"Single image shape: {test_image.shape} -> [color_channels, height, width]")
+# print(f"Single image pixel values:\n{test_image}")
+
+torch.manual_seed(42)
+
+# Create a convolutional layer with same dimensions as TinyVGG
+# (try changing any of the parameters and see what happens)
+conv_layer = nn.Conv2d(
+    in_channels=3, out_channels=10, kernel_size=3, stride=1, padding=0
+)  # also try using "valid" or "same" here
+
+# Pass the data through the convolutional layer
+print(
+    conv_layer(test_image)
+)  # Note: If running PyTorch <1.11.0, this will error because of shape issues (nn.Conv.2d() expects a 4d tensor as input)
+
+# Setup loss and optimizer
+loss_fn = nn.CrossEntropyLoss()
+optimizer = torch.optim.SGD(params=model_2.parameters(), lr=0.1)
+
+
+def train_step(
+    model: torch.nn.Module,
+    data_loader: torch.utils.data.DataLoader,
+    loss_fn: torch.nn.Module,
+    optimizer: torch.optim.Optimizer,
+    accuracy_fn,
+    device: torch.device = device,
+):
+    train_loss, train_acc = 0, 0
+    model.to(device)
+    for batch, (X, y) in enumerate(data_loader):
+        # Send data to GPU
+        X, y = X.to(device), y.to(device)
+
+        # 1. Forward pass
+        y_pred = model(X)
+
+        # 2. Calculate loss
+        loss = loss_fn(y_pred, y)
+        train_loss += loss
+        train_acc += accuracy_fn(
+            y_true=y, y_pred=y_pred.argmax(dim=1)
+        )  # Go from logits -> pred labels
+
+        # 3. Optimizer zero grad
+        optimizer.zero_grad()
+
+        # 4. Loss backward
+        loss.backward()
+
+        # 5. Optimizer step
+        optimizer.step()
+
+    # Calculate loss and accuracy per epoch and print out what's happening
+    train_loss /= len(data_loader)
+    train_acc /= len(data_loader)
+    print(f"Train loss: {train_loss:.5f} | Train accuracy: {train_acc:.2f}%")
+
+
+def test_step(
+    data_loader: torch.utils.data.DataLoader,
+    model: torch.nn.Module,
+    loss_fn: torch.nn.Module,
+    accuracy_fn,
+    device: torch.device = device,
+):
+    test_loss, test_acc = 0, 0
+    model.to(device)
+    model.eval()  # put model in eval mode
+    # Turn on inference context manager
+    with torch.inference_mode():
+        for X, y in data_loader:
+            # Send data to GPU
+            X, y = X.to(device), y.to(device)
+
+            # 1. Forward pass
+            test_pred = model(X)
+
+            # 2. Calculate loss and accuracy
+            test_loss += loss_fn(test_pred, y)
+            test_acc += accuracy_fn(
+                y_true=y,
+                y_pred=test_pred.argmax(dim=1),  # Go from logits -> pred labels
+            )
+
+        # Adjust metrics and print out
+        test_loss /= len(data_loader)
+        test_acc /= len(data_loader)
+        print(f"Test loss: {test_loss:.5f} | Test accuracy: {test_acc:.2f}%\n")
+
+
+from torch.utils.data import DataLoader
+from helper_functions import accuracy_fn, print_train_time
+from tqdm.auto import tqdm
+
+BATCH_SIZE = 32
+
+train_dataloader = DataLoader(train_data, batch_size=BATCH_SIZE, shuffle=True)
+
+test_dataloader = DataLoader(
+    test_data,
+    batch_size=BATCH_SIZE,
+    shuffle=False,  # don't necessarily have to shuffle the testing data
+)
+torch.manual_seed(42)
+
+# Measure time
+from timeit import default_timer as timer
+
+train_time_start_model_2 = timer()
+
+# Train and test model
+epochs = 3
+for epoch in tqdm(range(epochs)):
+    print(f"Epoch: {epoch}\n---------")
+    train_step(
+        data_loader=train_dataloader,
+        model=model_2,
+        loss_fn=loss_fn,
+        optimizer=optimizer,
+        accuracy_fn=accuracy_fn,
+        device=device,
+    )
+    test_step(
+        data_loader=test_dataloader,
+        model=model_2,
+        loss_fn=loss_fn,
+        accuracy_fn=accuracy_fn,
+        device=device,
+    )
+
+train_time_end_model_2 = timer()
+total_train_time_model_2 = print_train_time(
+    start=train_time_start_model_2, end=train_time_end_model_2, device=device
+)
